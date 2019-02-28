cb4b0b826bbdcae0ad055f4579411fdfa17e712a
Daniel Oberbillig <danieloberbillig@gmail.com>
2019-02-28 16:36:42 +1100

    Half way through with tutorial

--- projects/my-other-app/src/app/app.component.ts
+++ projects/my-other-app/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Data & Design';
-  someOtherText = '';
+  title = 'my-other-app';
 }
