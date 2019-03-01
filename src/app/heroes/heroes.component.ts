4f451eb467d223e11f2d7993e54cc4126315bad0
Daniel Oberbillig <danieloberbillig@gmail.com>
2019-03-01 15:43:00 +1100

    completed tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,38 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-// import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+// import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  // heroes = HEROES;
   heroes: Hero[];
-  
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
 
-  // synchronous operation:
-  // getHeroes(): void {
-  //   this.heroes = this.heroService.getHeroes();
-  // }
-  
-  constructor(private heroService: HeroService) {
-  
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+
 }
+
+// before: routing
+// export class HeroesComponent implements OnInit {
+//   selectedHero: Hero;
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//   }
+
+//   // heroes = HEROES;
+//   heroes: Hero[];
+
+//   getHeroes(): void {
+//     this.heroService.getHeroes()
+//         .subscribe(heroes => this.heroes = heroes);
+//   }
+
+//   // synchronous operation:
+//   // getHeroes(): void {
+//   //   this.heroes = this.heroService.getHeroes();
+//   // }
+
+//   constructor(private heroService: HeroService) {
+
+//   }
+
+//   ngOnInit() {
+//     this.getHeroes();
+//   }
+
+// }
